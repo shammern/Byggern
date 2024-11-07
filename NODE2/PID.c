53e41c2d967524155dd7f93de035522be5519de5
danienes <98825763+danienes@users.noreply.github.com>
2024-11-07 13:15:54 +0100

    Add files via upload

--- NODE2/PID.c
+++ NODE2/PID.c
@@ -1,6 +1,40 @@
-/*
- * PID.c
- *
- * Created: 06.11.2024 10:21:43
- *  Author: torstepu
- */ 
+#include "PID.h"
+
+static PID_parameters PID = {0,0,0,0,0,0,0};
+
+void pid_init(float k_p, float k_i, float k_d, float timestep, int max_u){
+    PID.max_u = max_u;
+    PID.T = timestep;
+    pid_set_parameters(k_d, k_i, k_d);
+}
+
+void pid_set_parameters(float k_p, float k_i, float k_d){
+    PID.K_d = k_d;
+    PID.K_i = k_i;
+    PID.K_p = k_p;
+}
+
+void pid_reset_error(){
+    PID.sum_error = 0;
+    PID.prev_error = 0;
+}
+
+int pid_controller(int ref, int curr_val){
+    int error = ref - curr_val;
+    PID.sum_error += error;
+
+    int u_p = PID.K_p * error;
+    int u_i = PID.T * PID.K_i * PID.sum_error;
+    int u_d = (PID.K_d / PID.T) * (error - PID.prev_error);
+
+    int u = u_p + u_i + u_d;
+    PID.prev_error = error;
+
+    if(u > PID.max_u){
+        u = PID.max_u;
+    }else if(u < -PID.max_u){
+        u = -PID.max_u;
+    }
+
+    return u;
+}
--- NODE2/PID.h
+++ NODE2/PID.h
@@ -1,16 +1,29 @@
-/*
- * PID.h
- *
- * Created: 06.11.2024 10:21:57
- *  Author: torstepu
- */ 
+#pragma once
+
+#include <stdint.h>
+
+#ifndef PID_H
+#define PID_H
+
+typedef struct
+{
+    float K_p;
+    float K_i;
+    float K_d;
+    int sum_error;
+    int prev_error;
+    float T;
+    int max_u;
+}PID_parameters;
+
+void pid_init(float k_p, float k_i, float k_d, float timestep, int max_u);
+
+void pid_set_parameters(float k_p, float k_i, float k_d);
+
+void pid_reset_error();
+
+int pid_controller(int ref);
 
 
-#ifndef PID_H_
-#define PID_H_
 
-
-
-
-
-#endif /* PID_H_ */
+#endif
